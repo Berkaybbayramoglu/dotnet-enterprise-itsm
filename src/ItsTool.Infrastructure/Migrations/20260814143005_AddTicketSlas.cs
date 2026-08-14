dda36f8c870daf00e58fbc8528fe4a0c2df1f705
berkaybbayramoglu <berkaybbayramoglu@gmail.com>
2026-08-14 17:38:40 +0300

    fixed login page

---
+++ src/ItsTool.Infrastructure/Migrations/20260814143005_AddTicketSlas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItsTool.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTicketSlas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
