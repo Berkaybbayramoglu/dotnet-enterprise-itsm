17a809475c311482b6376cccb8c2ff622f68afed
berkaybbayramoglu <berkaybbayramoglu@gmail.com>
2026-08-19 11:28:07 +0300

    fix: align field type enum mapping in admin UI

---
+++ src/ItsTool.API/Controllers/LookupController.cs
@@ -0,0 +1,39 @@
+using ItsTool.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItsTool.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class LookupController : ControllerBase
+{
+    private readonly ICatalogService _catalogService;
+    private readonly IProjectService _projectService;
+
+    public LookupController(ICatalogService catalogService, IProjectService projectService)
+    {
+        _catalogService = catalogService;
+        _projectService = projectService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetLookups()
+    {
+        var projects = await _projectService.GetAllAsync();
+        var categories = await _catalogService.GetCategoriesAsync(null);
+        var ticketTypes = await _catalogService.GetTicketTypesAsync();
+        var priorities = await _catalogService.GetPrioritiesAsync();
+        var statuses = await _catalogService.GetStatusesAsync();
+
+        return Ok(new
+        {
+            projects,
+            categories,
+            ticketTypes,
+            priorities,
+            statuses
+        });
+    }
+}
